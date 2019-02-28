78ea195674d0bcee5bae6dc25eaeeb2846ed9bce
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-28 08:10:27 -0500

    S2L8: Connecting to MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://juan:juan@devconnector-liyrv.mongodb.net/test?retryWrites=tru";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+module.exports = {
+  uri:
+    "mongodb+srv://juan:juan@devconnector-liyrv.mongodb.net/test?retryWrites=true"
+};
