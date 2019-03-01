a2906d3819a64bfb17953ead0c452329b8cd2358
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-01 16:00:41 -0500

    Specifying that profile is going to be Private

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
-// @access  Public
+// @access  Private with JWT
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
 
 module.exports = router;
