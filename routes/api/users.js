408bb866441dbd6cf2a35dd17bcb8776d1f9d5c6
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-01 18:19:48 -0500

    S3L10 | Hashing the password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -29,6 +30,18 @@ router.post("/register", (req, res) => {
         email: req.body.email,
         avatar,
         password: req.body.password
+      });
+
+      // Hashing the password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          (newUser.password = hash),
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+        });
       });
     }
   });
