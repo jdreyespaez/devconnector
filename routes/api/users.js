45bf5161a8d1e5c8cb49101f21b4b9dde8a295a2
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-01 18:04:27 -0500

    S3L10 | Using gravatar and defining it inside the new User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
 
 // Load User model
 const User = require("../../models/User");
@@ -17,6 +18,12 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default for the image
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
