4129157bf8126a4d545f8f1828090450fb276abe
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-03-01 19:11:33 -0500

    S3L10 | Hashing the password

--- routes/api/users.js
+++ routes/api/users.js
@@ -36,11 +36,11 @@ router.post("/register", (req, res) => {
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
-          (newUser.password = hash),
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
         });
       });
     }
